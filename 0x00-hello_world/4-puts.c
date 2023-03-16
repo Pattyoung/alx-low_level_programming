db250a9d6f15f83f4c9568828926c411d1dc880e
Patrice Okeyeochu <pattyoungok@gmail.com>
2023-03-16 09:31:54 -0700

    exe

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
  * main - this comment for first
- * return - to 0
+ * and return to 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+/**
+ * return - if suc to zero
+ */
 	return (0);
 }
