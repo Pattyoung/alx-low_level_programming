613e15362754a691879349cc4a3d464887fd592d
Patrice Okeyeochu <pattyoungok@gmail.com>
2023-03-16 09:26:18 -0700

    ythf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
- /** main - this comment for first
-  * return to 0
-  */
+/**
+ * main - this comment for first
+ * return to 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
