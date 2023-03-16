ac84af929417cc70ab02b6e1283099a1367b3f52
Patrice Okeyeochu <pattyoungok@gmail.com>
2023-03-16 10:24:48 -0700

    types sixe

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	printf("Size of a double: %lu byte(s)\n", sizeof(double));
+	printf("Size of long int: %lu byte(s)\n)", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
 	return (0);
 }
