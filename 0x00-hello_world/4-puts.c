a64a4289c37ab98290a3dad9dedfdc9972e865e4
Patrice Okeyeochu <pattyoungok@gmail.com>
2023-03-16 09:41:07 -0700

    al di

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
  * main - this comment for first
- * return - to 0
+ * Return: this returns 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-/**
- * return - if suc to zero
- */
 	return (0);
 }
