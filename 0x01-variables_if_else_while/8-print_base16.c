0449b3f2d6255fc55ba09cc7f2edc2e4b43c7e4a
Patrice Okeyeochu <pattyoungok@gmail.com>
2023-03-17 13:04:58 -0700

    b1

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+   * main - this mainfuc
+   * Return: this returns 0
+    */
+
+int main(void)
+{
+	int p;
+	char q;
+
+	for (p = 0; p < 10; p++)
+		putchar(p + '0');
+	for (q = 'a'; q < 'f'; q++)
+		putchar(q);
+	putchar('\n');
+	return (0);
+}
