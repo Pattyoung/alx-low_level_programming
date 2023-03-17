a092f289e52d44335dc75e9cfff719845c5f52f0
Patrice Okeyeochu <pattyoungok@gmail.com>
2023-03-17 15:02:01 -0700

    klf

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,21 +7,20 @@
 
 int main(void)
 {
-	int i, j
-		;
+	int i, j;
+
 	for (i = 0; i <= 8; i++)
 	{
 	for (j = i + 1; j <= 9; j++)
 	{
-		putchar('0' + i / 9 * j);
-		putchar('0' + (i % 9 + j) / 10);
-		putchar('0' + (i % 9 + j) % 10);
-		if (i != 8 || j != 9)
-			{
-			putchar(',');
-			putchar(' ');
-			}
+		putchar('0' + i);
+		putchar('0' + j);
+		{
+		putchar(',');
+		putchar(' ');
+		}
 	}
 	}
-return (0);
+	putchar('\n');
+	return (0);
 }
