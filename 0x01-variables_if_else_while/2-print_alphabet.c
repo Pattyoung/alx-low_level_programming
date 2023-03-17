5192d22b43cee47e512064c5eb1bd1cbbe492bcd
Patrice Okeyeochu <pattyoungok@gmail.com>
2023-03-17 11:17:34 -0700

    e2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main func
+ * Return: this will return 0
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a' ; n <= 'z' ; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
