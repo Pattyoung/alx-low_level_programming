261a601cd0cded091a4e3a9753fb5510ef28ff12
Patrice Okeyeochu <pattyoungok@gmail.com>
2023-03-17 12:31:37 -0700

    h1

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is main
+ * Return: this returns 0
+ */
+
+int main(void)
+{
+	int p;
+
+	for (p = 0; p < 10; p++)
+		putchar( p + '0');
+	putchar('\n');
+	return (0);
+}
