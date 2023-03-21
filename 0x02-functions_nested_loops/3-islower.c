ac04cdfc41449ee21f187855e090dae8a134825c
Patrice Okeyeochu <pattyoungok@gmail.com>
2023-03-21 08:45:01 -0700

    cp

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - this mainfuc
+ * @c: to be printed
+ * Return: this returns 1 or 0
+ */
+
+int _islower(int c);
+{
+	if (c <= 97 && c >= 122)
+		return (1);
+	else
+		return (0);
+}
