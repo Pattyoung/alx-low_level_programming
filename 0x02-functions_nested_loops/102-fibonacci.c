adf5a194270c76fd7192839ab323fd07137ff6ad
Patrice Okeyeochu <pattyoungok@gmail.com>
2023-03-22 10:16:10 -0700

    gf

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,16 +7,21 @@
 
 int main(void)
 {
-	int i, n = 50, t1 = 1, t2 = 2, nextTerm;
+	int i, n = 50;
+	long long int t1 = 1, t2 = 2, nextTerm;
 
-	printf("%d, %d, ", t1, t2);
+	printf("%lld, %lld, ", t1, t2);
 
 	for (i = 3; i <= n; i++)
 	{
 		nextTerm = t1 + t2;
 		t1 = t2;
 		t2 = nextTerm;
-		printf("%d, ", nextTerm);
+		printf("%lld", nextTerm);
+	if (i < n)
+	{
+		printf(", ");
+	}
 	}
 	printf("\n");
 	return (0);
