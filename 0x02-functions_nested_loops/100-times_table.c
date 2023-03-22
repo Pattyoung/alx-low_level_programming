5bfcd458448826e2cf3f187c2f39cce7232ffd65
Patrice Okeyeochu <pattyoungok@gmail.com>
2023-03-22 09:29:15 -0700

    cmt

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,15 +8,19 @@
 
 void print_times_table(int n)
 {
+	int i;
+	int j;
+	int result;
+
 	if (n < 0 || n > 15)
 	{
 	return;
 	}
-	for (int i = 0; i <= n; i++)
+	for (i = 0; i <= n; i++)
 	{
-	for (int j = 0; j <= n; j++)
+	for (j = 0; j <= n; j++)
 	{
-	int result = i * j;
+	result = i * j;
 
 	if (j == 0)
 	{
