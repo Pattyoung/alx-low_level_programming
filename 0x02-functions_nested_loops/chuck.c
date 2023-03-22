6196ca058c19493dd9ef21fd70fdf3169b5c2030
Patrice Okeyeochu <pattyoungok@gmail.com>
2023-03-22 07:39:58 -0700

    koi

--- 0x02-functions_nested_loops/chuck.c
+++ 0x02-functions_nested_loops/chuck.c
@@ -1,18 +1,52 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - this mainfuc
+ * @n: parametre
+ * Return: this returns 0
+ */
 
 void print_to_98(int n)
 {
-    printf("%d", n);
-    if (n < 98) {
-        for (int i = n+1; i <= 98; i++) {
-            printf(", %d", i);
-        }
-    } else if (n > 98) {
-        for (int i = n-1; i >= 98; i--) {
-            printf(", %d", i);
-        }
-    }
-    printf("\n");
-}
+	int i;
 
+	if (n >= 0 && n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			{
+			if (i != 98)
+				printf(", ");
+		}
+		}
+		printf("\n");
+	}
+	else if (n < 0)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			{
+			if (i != 98)
+				printf(", ");
+		}
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			{
+			if (i != 98)
+				printf(", ");
+		}
+		}
+		printf("\n");
+	}
+	else if (n == 98)
+		printf("%d", n);
+}
