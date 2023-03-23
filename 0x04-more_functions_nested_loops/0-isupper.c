c82968cbb67177832ede39e008649083062f4e2c
Patrice Okeyeochu <pattyoungok@gmail.com>
2023-03-23 07:35:35 -0700

    mmm

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - mainfunc
+ * @x: to be checked
+ * Return: 1 or 0
+ */
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return(1);
+	}
+	return(0);
+}
