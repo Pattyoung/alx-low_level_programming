462b70ec3a7422e1e5664f452139f1d13015ae26
Patrice Okeyeochu <pattyoungok@gmail.com>
2023-03-23 08:52:36 -0700

    llk

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,17 +9,16 @@
 void print_most_numbers(void)
 {
 	int x = 0;
-
-	for (x <= 9; x++)
+	for (c = 48; c < 58; c++)
 	{
-	if (x == 2 || x == 4)
+	if (c != 50)
 	{
-	continue;
+	if (c != 52)
+	{
+	_putchar(c);
 	}
-	else
-	{
-	putchar(x + '0');
 	}
 	}
 	_putchar('\n');
 }
+
